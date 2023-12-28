089e5f7004d31ce7fa6f456e130b906982ce3fcb
anirudhj2000 <anirudhjoshi985@gmail.com>
2023-12-28 22:38:43 +0530

    first commit

---
+++ src/pullswitch.js
@@ -0,0 +1,12 @@
+import { View, Text, TouchableOpacity } from "react-native";
+
+const PullSwitchDemo = () => {
+  return (
+    <View>
+      <Text>Hi</Text>
+      <TouchableOpacity></TouchableOpacity>
+    </View>
+  );
+};
+
+export default PullSwitchDemo;
