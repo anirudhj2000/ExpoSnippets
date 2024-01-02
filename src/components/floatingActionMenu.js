ce1451e75f411041780b04554b8b4978f49771c4
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-01-02 21:39:25 +0530

    floating button menu

---
+++ src/components/floatingActionMenu.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+import Animated, {
+  withSpring,
+  useSharedValue,
+  useAnimatedStyle,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
