4e14775e253893de43892e052fa3d4981001089e
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-01-02 21:39:53 +0530

    config and setup changes

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import RetractingSwitchDemo from "./src/screens/retractingSwitchDemo";
+import FloatingActionButtonDemo from "./src/screens/floatingActionMenuDemo";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={styles.container}>
-        <RetractingSwitchDemo />
+        <FloatingActionButtonDemo />
       </View>
     </GestureHandlerRootView>
   );
